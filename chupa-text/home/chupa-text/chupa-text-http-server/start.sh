12e67242bae73835ea221783175e71596a92c0d0
Kouhei Sutou <kou@clear-code.com>
2017-07-11 16:02:30 +0900

    Make workable

---
+++ chupa-text/home/chupa-text/chupa-text-http-server/start.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+#
+# Copyright (C) 2017  Kouhei Sutou <kou@clear-code.com>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+set -e
+set -u
+
+sudo -H chown -R $(id --name --user):$(id --name --group) log
+
+(echo "production:"; \
+ echo "  secret_key_base: $(bin/rails secret)") > \
+  config/secrets.yml
+
+xvfb-run bin/rails server
